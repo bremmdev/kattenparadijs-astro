0ea49de35e256778b5ca41c9e22f7f18b84422b2
bremmdev <bremmdev@gmail.com>
2026-03-14 10:08:56 +0100

    add header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Dancing Script",
+      cssVariable: "--font-dancing-script",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Poppins",
+      weights: [400, 500, 700],
+      cssVariable: "--font-poppins",
+    },
+  ],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
