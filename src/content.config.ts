cb56aed9f56ee378a48be4c110cedf8f66259504
bremmdev <bremmdev@gmail.com>
2026-03-18 18:19:37 +0100

    render images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,14 @@
 import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
 import { sanityClient } from './sanity';
 import { imageGroqQuery } from './utils/query';
-
-const Cat = z.object({
-    name: z.string(),
-    birthDate: z.string(),
-    passingDate: z.string().nullable(),
-    iconUrl: z.string(),
-    nicknames: z.array(z.string()),
-});
-
-const Image = z.object({
-    cats: z.array(Cat),
-    width: z.number(),
-    height: z.number(),
-    id: z.string(),
-    url: z.string(),
-    takenAt: z.string().nullable(),
-    blurData: z.string(),
-});
+import { ImageSchema } from './utils/types';
 
 const images = defineCollection({
     loader: async () => {
         const data = await sanityClient.fetch(imageGroqQuery({ page: 0 }));
-        return data.images;
+        return data.images
     },
-    schema: Image,
+    schema: ImageSchema,
 });
 
 // Expose your defined collection to Astro
---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+import { z } from "astro/zod";
+
+export const CatSchema= z.object({
+    name: z.string(),
+    birthDate: z.string(),
+    passingDate: z.string().nullable(),
+    iconUrl: z.string(),
+    nicknames: z.array(z.string()),
+});
+
+export const ImageSchema = z.object({
+    cats: z.array(CatSchema),
+    width: z.number(),
+    height: z.number(),
+    id: z.string(),
+    url: z.string(),
+    takenAt: z.string().nullable(),
+    blurData: z.string(),
+});
+
+export type Cat = z.infer<typeof CatSchema>;
+export type ImageWithDimensions = z.infer<typeof ImageSchema>;
